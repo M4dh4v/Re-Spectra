d69103734a917db131798ec26446c893972366da
M4dh4v <discordboosterm@gmail.com>
2025-08-12 22:51:44 +0530

    Updated baseurl

--- Frontend/src/baseurl.js
+++ Frontend/src/baseurl.js
@@ -1,4 +1,4 @@
 // require('dotenv').config();
 
-export const baseUrl = 'http://localhost:5000';
-// export const baseUrl = '';
+// export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'https://https://re-spec-back.vercel.app';
